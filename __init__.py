9d281ca869b425194380883168183b48e94b0d1d
RIDLEYsan <excalibur.bluesilver.801a@gmail.com>
2024-10-29 21:58:29 +0900

    Update .gitignore to ignore specific app files

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from . import concept_app
+from . import design_app
+
+# If you want to make the run functions directly accessible from the apps package,
+# you can uncomment the following lines:
+# from .concept_app import run as run_concept_app
+# from .design_app import run as run_design_app
